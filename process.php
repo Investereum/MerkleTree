f8108e23b252b2f4c310f6e7af6dbc28fd8b8fc2
Pieter Aertgeerts <pieter@investereum.be>
2018-12-20 16:28:31 +0100

    Update process.php

--- process.php
+++ process.php
@@ -1,7 +1,7 @@
 <?php
 // Configure your Subject Prefix and Recipient here
 $subjectPrefix = 'aSaas HTML Template';
-$emailTo       = 'ashekurrahman1@gmail.com';
+$emailTo       = 'pieteraertgeerts@gmail.com';
 $errors = array(); // array to hold validation errors
 $data   = array(); // array to pass back data
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
